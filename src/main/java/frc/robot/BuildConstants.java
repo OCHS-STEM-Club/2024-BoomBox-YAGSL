e6193378ab13606abf4c2e7e81617a9c601ac04f
Prince <princerodrick59@gmail.com>
2024-07-15 19:50:55 -0500

    put speed down to practice speed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-BoomBox-YAGSL";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 19;
-  public static final String GIT_SHA = "811a798d110c6a99816e47d809a2090ee814b273";
-  public static final String GIT_DATE = "2024-06-17 18:59:24 CDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-06-17 19:00:54 CDT";
-  public static final long BUILD_UNIX_TIME = 1718668854475L;
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "5d1c51d49d424c3a9213b18b87b2f3cdd4c40afb";
+  public static final String GIT_DATE = "2024-06-20 18:18:09 CDT";
+  public static final String GIT_BRANCH = "Lazer-Lights-2024";
+  public static final String BUILD_DATE = "2024-07-15 19:43:26 CDT";
+  public static final long BUILD_UNIX_TIME = 1721090606935L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
