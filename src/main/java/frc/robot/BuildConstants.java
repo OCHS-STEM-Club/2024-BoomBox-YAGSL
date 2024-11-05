8076103493d48c1767413ed1233493eafde023f6
Prince <princerodrick59@gmail.com>
2024-11-04 19:46:22 -0600

    tuned boombox, l3 modules work

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-BoomBox-YAGSL";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "24e57396eebfcfd0984ab01f0c601d491bad98fe";
-  public static final String GIT_DATE = "2024-07-29 19:47:18 CDT";
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "73498f15a0d9d714d8e47d1249c7866b9724db42";
+  public static final String GIT_DATE = "2024-10-31 20:12:23 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-10-31 20:07:32 CDT";
-  public static final long BUILD_UNIX_TIME = 1730423252940L;
+  public static final String BUILD_DATE = "2024-11-04 19:41:18 CST";
+  public static final long BUILD_UNIX_TIME = 1730770878908L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -51,7 +51,7 @@ public class SwerveSubsystem extends SubsystemBase
   /**
    * Maximum speed of the robot in meters per second, used to limit acceleration.
    */
-  public        double      maximumSpeed = Units.feetToMeters(16.6);
+  public        double      maximumSpeed = Units.feetToMeters(14.5);
 
   /**
    * Initialize {@link SwerveDrive} with the directory provided.
